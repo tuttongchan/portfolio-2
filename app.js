6f9efa862d3a137dc6d4479f0c4878c0009a9624
Tut Tongchantra <tuttongchan@Tuts-MacBook-Air.local>
2021-01-10 20:06:19 -0800

    AOS animations

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-alert('Site is currently undergoing maintenance and isnt mobile responsive yet, but some functionality still works. So feel free to browse! Enjoy :)');
+AOS.init({
+  delay: 50, // values from 0 to 3000, with step 50ms
+  duration: 1000, // values from 0 to 3000, with step 50ms
+  once: false, // whether animation should happen only once - while scrolling down
+  mirror: true, // whether elements should animate out while scrolling past them
+  anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+
+//   disable: 'mobile'
+});
